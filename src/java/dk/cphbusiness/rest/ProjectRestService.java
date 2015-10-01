65b46d58e61a80ac1a4a9183c791b950fd1c0119
Jeanette <Jeanette@Jeanette-Pc>
2015-10-01 21:31:09 +0200

    har næsten lavet alle metoderne, udover add user to project da jeg ikke er sikker på hvordan jeg skal lave den

--- src/java/dk/cphbusiness/rest/ProjectRestService.java
+++ src/java/dk/cphbusiness/rest/ProjectRestService.java
@@ -8,14 +8,20 @@ package dk.cphbusiness.rest;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import dk.cphbusiness.entity.Facade;
+import dk.cphbusiness.entity.Project;
+import dk.cphbusiness.entity.ProjectUser;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * REST Web Service
@@ -23,7 +29,8 @@ import javax.ws.rs.PUT;
  * @author sofus
  */
 @Path("projects")
-public class ProjectRestService {
+public class ProjectRestService
+{
 
     @Context
     private UriInfo context;
@@ -31,13 +38,48 @@ public class ProjectRestService {
     /**
      * Creates a new instance of RestService
      */
-Gson gson;
-    public ProjectRestService() {
-    gson = new GsonBuilder().setPrettyPrinting().setFieldNamingPolicy(FieldNamingPolicy.IDENTITY).create();
-        
+    Gson gson;
+
+    public ProjectRestService()
+    {
+        gson = new GsonBuilder().setPrettyPrinting().setFieldNamingPolicy(FieldNamingPolicy.IDENTITY).create();
+
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createProject(String project)
+    {
+        Project p = gson.fromJson(project, Project.class);
+        Facade.createProject(p);
+        return Response.status(Response.Status.CREATED).type(MediaType.APPLICATION_JSON).entity(gson.toJson(p)).build();
+    }
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllProjects()
+    {
+        return Response.status(Response.Status.OK).entity(gson.toJson(Facade.getAllProjects())).build();
+    }
+
+    @GET
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProject(@PathParam("id") long id)
+    {
+        return Response.status(Response.Status.OK).entity(gson.toJson(Facade.findProject(id))).build();
+    }
+
+    @POST
+    @Path("{projectID}/{userID}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response AssignAUserToAProject (@PathParam("projectID")long projectID, @PathParam("userID")long userID)
+    {
+        return null;
     }
     
-    
-
-   
 }
